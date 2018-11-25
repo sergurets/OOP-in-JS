dbe381ff44758c0abee7063bf6d5b53cd893b590
Sergiy Smituh <sergurets@gmail.com>
2018-11-25 23:39:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,20 @@
-const obj1 = {
-    sayHi: () => console.log('Hi!')
+function Animal (breed) {
+  this.getBreed = function() {
+    console.log(breed)
   };
-  
-  const obj2 = {
-    showNumber: () => console.log(parseFloat(Math.random() * 100))
-  };
-  
-  const obj3 = {
-    showRandomSymbol: () => console.log(String.fromCharCode(parseFloat(Math.random() * 100)))
-  }; 
-  
-  const bigFatObject = makeChain(obj1, obj2, obj3);
-  
-  function makeChain () {
-    if (!arguments.length) {
-      return {}
-    }
-    var args = Array.prototype.slice.call(arguments);
-    var obj = args[args.length - 1];
-    args.splice(-1);
-    args.reduceRight(function(obj, current) {
-      obj.__proto__ = current;
-      return obj.__proto__
-    }, obj);
-    return obj;
+}
+
+function Dog (breed) {
+  if (!(this instanceof Dog)) {
+    return new Dog(breed);
   }
-  
-  console.log('bigFatObject', bigFatObject);
-  bigFatObject.sayHi();
-  bigFatObject.showNumber();
-  bigFatObject.showRandomSymbol();
+  Animal.apply(this, arguments);
+}
+
+const haski = new Dog('haski');
+const pitBul = Dog('pitBull');
+haski.getBreed(); // haski
+// still works
+pitBul.getBreed(); // pitBull
+console.log(pitBul, haski);
+console.log(pitBul instanceof Dog) // true
